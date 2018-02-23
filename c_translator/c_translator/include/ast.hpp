d5be14ea3d9bb6e388a6d20c7b9f8fbf7beb4701
t-tht <tht216@ic.ac.uk>
2018-02-23 16:57:12 +0000

    update

--- c_translator/c_translator/include/ast.hpp
+++ c_translator/c_translator/include/ast.hpp
@@ -18,13 +18,6 @@ public:
     //! Tell and expression to print itself to the given stream
     virtual void print(std::ostream &dst) const =0;
 
-	/*
-    //! Evaluate the tree using the given mapping of variables to numbers
-    virtual double evaluate(
-        const std::map<std::string,double> &bindings
-    ) const
-    { throw std::runtime_error("Not implemented."); }
-	*/
 };
 // ---------------------------------PROGRAM---------------------------------
 
@@ -37,13 +30,15 @@ class FunctionDec : public Node {
 		StatementPtr body;
 	public:
 		
-		FunctionDec(const std::string *type, const std::string *id, StatementPtr body);
+		FunctionDec(const std::string *type_in, const std::string *id_in, StatementPtr body_in);
 		~FunctionDec();
 		
 		void print() const override;
 }
-FunctionDec::FunctionDec(const std::string *type, const std::string *id, StatementPtr body){}
+
+FunctionDec::FunctionDec(const std::string *type_in, const std::string *id_in, StatementPtr body_in):type(type_in),id(id_in),body(body_in){}
 FunctionDec::~FunctionDec(){}
+
 // ---------------------------------EXPRESSION---------------------------------
 
 // ---------------------------------STATEMENT---------------------------------
