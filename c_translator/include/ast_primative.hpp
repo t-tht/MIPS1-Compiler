9ada5b53fade2df5b5021e3c6e8181e83e65e879
t-tht <tht216@ic.ac.uk>
2018-03-03 20:13:47 +0000

    binexp.hpp primative.hpp created

---
+++ c_translator/include/ast_primative.hpp
@@ -0,0 +1,38 @@
+#ifndef ast_primative_hpp
+#define ast_primative_hpp
+
+class Number;
+class Variable;
+
+class Number : public Node{
+	private:
+		double value;
+	public:
+		Number(double _value) : value(_value){};
+		~Number(){};
+		double getval() const{
+			return value;
+		};
+		virtual void translate(std::ostream &dst)const override{
+			dst << value;
+		};
+		virtual void print(std::ostream &dst)const override{
+			dst << value;
+		};
+};
+
+class Variable : public Node{
+	private:
+		std::string* id;
+	public:
+		Variable(double _id) : id(_id){};
+		~Variable(){};
+		virtual void translate(std::ostream &dst)const override{
+			dst << *id;
+		};
+		virtual void print(std::ostream &dst)const override{
+			dst << *id;
+		};
+};
+
+#endif
