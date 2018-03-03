3f1ce9827c77b46eb387f4660b68a23b24be6081
t-tht <tht216@ic.ac.uk>
2018-03-03 19:32:47 +0000

    revert

--- c_translator/src/c_parser.tab.hpp
+++ c_translator/src/c_parser.tab.hpp
@@ -40,7 +40,7 @@
 extern int yydebug;
 #endif
 /* "%code requires" blocks.  */
-#line 1 "src/c_parser.y" /* yacc.c:1915  */
+#line 1 "src/c_parser.y" /* yacc.c:1909  */
 
   #include "ast.hpp"
 
@@ -54,7 +54,7 @@ extern int yydebug;
   int yylex(void);
   void yyerror(const char *);
 
-#line 58 "src/c_parser.tab.hpp" /* yacc.c:1915  */
+#line 58 "src/c_parser.tab.hpp" /* yacc.c:1909  */
 
 /* Token type.  */
 #ifndef YYTOKENTYPE
@@ -81,13 +81,13 @@ extern int yydebug;
 
 union YYSTYPE
 {
-#line 17 "src/c_parser.y" /* yacc.c:1915  */
+#line 17 "src/c_parser.y" /* yacc.c:1909  */
 
   const Node *node;
   double number;
   std::string *string;
 
-#line 91 "src/c_parser.tab.hpp" /* yacc.c:1915  */
+#line 91 "src/c_parser.tab.hpp" /* yacc.c:1909  */
 };
 
 typedef union YYSTYPE YYSTYPE;
