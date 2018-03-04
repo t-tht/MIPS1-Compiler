595cc00956757986aa97f40fc263c5b06a1c31f1
keerthanenr <keerthanenr@live.co.uk>
2018-03-04 15:25:18 +0000

    Next part
    We've known each other for so long
    Your heart's been aching but you're too shy to say it
    Inside we both know what's been going on
    We know the game and we're gonna play it

---
+++ c_translator/include/ast_funccallexpr.hpp
@@ -0,0 +1,33 @@
+#ifndef ast_funccallexpr_hpp
+#define ast_funccallexpr_hpp
+
+#include<string>
+#include<iostream>
+#include<ast.hpp>
+
+class FuncCallExpr;
+
+class FuncCallExpr: public Node{
+	protected:
+		std::string* id;
+		NodePtr param;		//needs fixing later, but atm assume no
+	public:
+		FuncCallExpr(std::string* _id, NodePtr _param): id(_id), param(_param){}
+		~FuncCallExpr(){
+			delete param;
+		}
+		void translate(std::ostream &dst) const{
+			dst << *id << "()" << std::endl;
+			if(param != NULL){
+				param ->translate(dst);
+			}
+		}
+		void print(std::ostream &dst) const {
+			dst << "print not implemented yet" <<std::endl;
+			if(param != NULL){
+				param ->print(dst);
+			}
+		}
+};
+
+#endif
