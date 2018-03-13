58e737affe6d1906bcce2a23cd6b78b7bd265ea2
keerthanenr <keerthanenr@live.co.uk>
2018-03-13 14:06:21 +0000

    433434

--- c_translator/include/ast_interpretcontext.hpp
+++ c_translator/include/ast_interpretcontext.hpp
@@ -5,43 +5,82 @@
 #include <iostream>
 #include <unordered_map>
 #include <string>
-#include "ast_global.hpp"
+#include <sstream>
 
 class InterpretContext;
 
+/*
+ Convention
+ |Name    | Reg# |      Usage          |Preserved on call?
+ |$zero   | 0    |the constant value 0 |n.a.
+ |$v0-$v1 | 2-3  |values for results   |no
+ |$a0-$a3 | 4-7  |arguments            |yes
+ |$t0-$t7 | 8-15 |temporaries          |no
+ |$s0-$s7 | 16-23|saved                |yes
+ |$t8-$t9 | 24-25|more temporaries     |no
+ |$gp     | 28   |global pointer       |yes
+ |$sp     | 29   |stack pointer        |yes
+ |$fp     | 30   |frame pointer        |yes
+ |$ra     | 31   |return address       |yes
+ 
+ */
+
 class InterpretContext{
 public:
-	unsigned int sp;
-	unsigned int frame_size;
-
-	int argno;
-	int paramno;
-
-	bool reg[32];	//check if register is free or occupied
-
-	std::unordered_map<std::string, unsigned int> variablebindings;
-	//TODO INCLUDE DYNAMIC BINDINGS IF NEEDED
-	std::unordered_map<std::string, unsigned int> globalbindings;
-
-	InterpretContext(){
-		for(int i = 0; i < 32; i++){
-			reg[i] = false;
-		}
-		sp = 0;
-		frame_size = 0;
-		argno = 0;
-		paramno = 4;
-	};
-	~InterpretContext(){};
-
-	void regsetfree(){};
-	void regsetused(){};
-
-	void addvar(const std::string* name){};
-
-
-
-
+    unsigned int stack_pointer;
+    unsigned int frame_point;
+    
+    unsigned int frame_size;
+    
+    
+    int argument_no;
+    int variable_no;
+    
+    
+    //REGISTER METHODS
+    //Declaring Registers
+    bool reg[32];
+    
+    //Generating free registers
+    InterpretContext(){
+        for(int i = 0; i < 32; i++){
+            reg[i] = false;
+        }
+        for(int i = 0; i < 8; i+){
+            reg[i]= true;
+        }
+        for(int i = 26; i < 32; i++){
+            reg[i]= true;
+        }
+        
+        
+        
+    };
+    
+    
+    
+    //Declaring Binding Map
+    std::unordered_map<std::string, unsigned int> VariableBindings;
+    std::unordered_map<std::string, unsigned int> DynamicBindings;
+    //TODO INCLUDE DYNAMIC BINDINGS IF NEEDED
+    //std::unordered_map<std::string, unsigned int> globalbindings
+    
+    //Create function to insert a binding on the stack
+    //
+    InterpretContext(InterpretContext* ctxt){
+        
+    }
+    ~InterpretContext(){};
+    
+    void regsetfree(){};
+    void regsetused(){};
+    
+    void addvar(const std::string* name){};
+    
+    
+    
+    
 };
 
 #endif
+
