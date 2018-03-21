d00a3fb1d6dd0124c128a525cb395e9f6c2f6350
t-tht <tht216@ic.ac.uk>
2018-03-21 16:10:17 +0000

    temp  commit

--- include/ast_funccallexpr.hpp
+++ include/ast_funccallexpr.hpp
@@ -6,30 +6,38 @@
 class FuncCallExpr;
 
 class FuncCallExpr: public Node{
-	protected:
-		std::string* id;
-		NodePtr param;		//needs fixing later, but atm assume no
-	public:
-		FuncCallExpr(std::string* _id, NodePtr _param): id(_id), param(_param){};
-		~FuncCallExpr(){
-			delete id;
-			delete param;
-		};
-		void translate(std::ostream &dst) const override{
-			dst << *id << "(";
-			if(param != NULL){
-				param->translate(dst);
-			}
-			dst << ")";
+protected:
+	std::string* id;
+	NodePtr param;		//needs fixing later, but atm assume no
+public:
+	FuncCallExpr(std::string* _id, NodePtr _param): id(_id), param(_param){};
+	~FuncCallExpr(){
+		delete id;
+		delete param;
+	};
+	void translate(std::ostream &dst) const override{
+		dst << *id << "(";
+		if(param != NULL){
+			param->translate(dst);
+		}
+		dst << ")";
 
-		};
-		void compile(std::ostream &dst, InterpretContext &cntx, unsigned int destloc) const override{
-			//cntx.functionLevelIncrement();
-			dst << "\tjal\t" << *id << std::endl;
-			//cntx.functionLevelDecrement();
-			dst << "\tnop" << std::endl;
-			dst << "\tmove\t$" << destloc << ", $2" << std::endl;
-		};
+	};
+	void compile(std::ostream &dst, InterpretContext &cntx, unsigned int destloc) const override{
+		if(param != NULL){
+			std::ostream temp(NULL);
+			param->compile(temp, cntx, destloc);
+			int param_no = cntx.paramCount();
+			for(int i = 0; i < param_no; i++){
+				dst << "\tlw\t\t$" << 4+i << ", " << /*fp offset*/ << "($fp)" << std::endl;
+			}
+		}
+		//cntx.functionLevelIncrement();
+		dst << "\tjal\t" << *id << std::endl;
+		//cntx.functionLevelDecrement();
+		dst << "\tnop" << std::endl;
+		dst << "\tmove\t$" << destloc << ", $2" << std::endl;
+	};
 };
 
 #endif
