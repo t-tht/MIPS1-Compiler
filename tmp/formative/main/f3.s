1d336f30b96fec44a7032a448e27b20e87ea3c75
keerthanenr <keerthanenr@live.co.uk>
2018-03-28 12:06:38 +0100

    feref

---
+++ tmp/formative/main/f3.s
@@ -0,0 +1,54 @@
+
+
+	.text
+	.align	2
+	.globl	main
+	.set	nomips16
+	.set	nomicromips
+	.ent	main
+	.type	main, @function
+main:
+	.frame	$fp, 128, $ra
+	.mask	0x40000000, -4
+	.fmask	0x00000000, 0
+	.set	noreorder
+	.set	nomacro
+
+#allocate stack
+	addiu	$sp, $sp, -128
+	sw		$ra, 124($sp)
+	sw		$fp, 120($sp)
+	move	$fp, $sp
+
+#compiling function body
+#binary expression--start
+#binary expression--start
+	li		$2, 10
+#binary expression--start
+	li		$3, 10
+	li		$8, 2
+	mult	$3, $8
+	mflo	$3
+#binary expression--end
+	addu	$2, $2, $3
+#binary expression--end
+	li		$2, 4
+	subu	$2, $2, $2
+#binary expression--end
+
+#deallocating stack
+	move	$sp, $fp
+	lw		$fp, 120($sp)
+	lw		$ra, 124($sp)
+	addiu	$sp, $sp, 128
+	j		$ra
+	nop
+
+	.set	macro
+	.set	reorder
+	.end	main
+	.size	main, .-main
+#Stack : 
+#Local Variable : 
+#compile finished
+#Global Variables : 
