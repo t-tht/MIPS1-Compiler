98f07be730d4779267a006319781ebcd1828cfa2
keerthanenr <keerthanenr@live.co.uk>
2018-03-29 18:23:51 +0100

    knjnkl

--- test_deliverable2.sh
+++ test_deliverable2.sh
@@ -15,30 +15,30 @@ NAME=$(basename $DRIVER _driver.c)
 TESTCODE=test_deliverable/test_cases/$NAME.c
 
 >&2 echo "Test case $NAME"
-
+>&2 echo "Test case $TESTCODE"
 # Compile driver with normal GCC
-mips-linux-gnu-gcc -c $DRIVER -o working/${NAME}_driver.o 2> working/${NAME}_driver.compile.stderr
+mips-linux-gnu-gcc -c $DRIVER -o working/${NAME}_driver.o
 if [[ $? -ne 0 ]]; then
 >&2 echo "                                                              ERROR : Couldn't compile driver program using GCC."
 continue
 fi
 
 # Compile test function with compiler under test to assembly
-$COMPILER -S $TESTCODE working/$NAME.s  2> working/${NAME}.compile.stderr
+$COMPILER -S $TESTCODE -o working/$NAME.s
 if [[ $? -ne 0 ]]; then
 >&2 echo "                                                              ERROR : Compiler returned error message."
 continue
 fi
 
 # Link driver object and assembly into executable
-mips-linux-gnu-gcc -static working/${NAME}.s working/${NAME}_driver.o -o working/${NAME}.elf 2> working/${NAME}.link.stderr
+mips-linux-gnu-gcc -static working/${NAME}.s working/${NAME}_driver.o -o working/${NAME}
 if [[ $? -ne 0 ]]; then
 >&2 echo "                                                              ERROR : Linker returned error message."
 continue
 fi
 
 # Run the actual executable
-qemu-mips working/${NAME}.elf
+qemu-mips working/${NAME}
 result=$?
 if [[ $result -ne 0 ]]; then
 >&2 echo "                                                              ERROR : Testcase returned $result, but expected 0."
