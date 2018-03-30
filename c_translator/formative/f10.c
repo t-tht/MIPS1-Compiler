cf6d24bec36814b6244bfa58310605cdcaadced4
keerthanenr <keerthanenr@live.co.uk>
2018-03-30 20:46:31 +0100

    final

--- c_translator/formative/f10.c
+++
@@ -1,8 +0,0 @@
-int f(int x)
-{
-    if(x>1){
-        return x*f(x);
-    }else{
-        return x;
-    }
-}
--- c_translator/formative/f10.py
+++
@@ -1,5 +0,0 @@
-def f(x):
-    if x>1:
-        return x*f(x)
-    else:
-        return x
